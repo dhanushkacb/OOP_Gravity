dc690ad0065b533c5b6aa288477574cacaa8f368
dhanushkacb <42224507+dhanushkacb@users.noreply.github.com>
2025-09-21 16:19:52 +0530

    Teachers schema

--- Src/db/Schema.py
+++ Src/db/Schema.py
@@ -34,3 +34,47 @@ class Users:
                 db_cursor.execute("SELECT user_id, username, role, created_at FROM users")
                 users = db_cursor.fetchall()
         return users
+    
+    def delete_user(self, user_id):
+        with Connection.Database() as db_conn:
+            with db_conn.cursor() as db_cursor:
+                db_cursor.execute("DELETE FROM users WHERE user_id = %s", (user_id,))
+            db_conn.commit()
+            return True
+        
+    def change_password(self, user_id, new_password):
+        new_password_hash = Security.hash(new_password)
+        with Connection.Database() as db_conn:
+            with db_conn.cursor() as db_cursor:
+                db_cursor.execute("UPDATE users SET password_hash = %s WHERE user_id = %s", (new_password_hash, user_id))
+            db_conn.commit()
+            return True
+        
+class Teachers:
+    def __init__(self):
+        pass
+
+    def add_teacher(self, name, subject, contact_no=None, email=None):
+        with Connection.Database() as db_conn:
+            with db_conn.cursor() as db_cursor:
+                db_cursor.execute(
+                    "INSERT INTO teachers (name, subject, contact_no, email) VALUES (%s, %s, %s, %s)",
+                    (name, subject, contact_no, email)
+                )
+            db_conn.commit()
+            return True
+    
+    def get_all_teachers(self):
+        with Connection.Database() as db_conn:
+            with db_conn.cursor() as db_cursor:
+                db_cursor.execute("SELECT * FROM teachers")
+                teachers = db_cursor.fetchall()
+        return teachers
+    
+    def delete_teacher(self, teacher_id):
+        with Connection.Database() as db_conn:
+            with db_conn.cursor() as db_cursor:
+                db_cursor.execute("DELETE FROM teachers WHERE teacher_id = %s", (teacher_id,))
+            db_conn.commit()
+            return True
+        
