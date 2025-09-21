d543142bfc268657e819f8b59bc682818ceee9e0
dhanushkacb <42224507+dhanushkacb@users.noreply.github.com>
2025-09-21 15:12:06 +0530

    Users schema

--- Src/db/Schema.py
+++ Src/db/Schema.py
@@ -1,5 +1,25 @@
 #create table store schema
-USERS = {
-    "admin": {"password": "admin123", "role": "Admin"},
-    "staff": {"password": "staff123", "role": "Staff"}
-}
+from Src.crypt import Security
+from Src.db.Connection import Connection
+
+
+class Users:
+    
+    def __init__(self):
+        pass
+
+    def authenticate(self, username, password):
+        with Connection.Database() as db_conn:
+            with db_conn.cursor() as db_cursor:
+                db_cursor.execute("SELECT role FROM users WHERE username = %s AND password_hash = %s", (username, Security.hash(password)))
+                result = db_cursor.fetchone()
+        if result:
+            return result[0]
+        return None
+    
+    def get_all_users(self):
+        with Connection.Database() as db_conn:
+            with db_conn.cursor() as db_cursor:
+                db_cursor.execute("SELECT user_id, username, role, created_at FROM users")
+                users = db_cursor.fetchall()
+        return users
