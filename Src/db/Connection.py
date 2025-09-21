09bc02128196a4a9d9ea18377fcca3c66758a5e2
dhanushkacb <42224507+dhanushkacb@users.noreply.github.com>
2025-09-21 11:43:56 +0530

    db connections

--- Src/db/Connection.py
+++ Src/db/Connection.py
@@ -1,22 +1,22 @@
 #handle db connection and return when required
 import mysql.connector
 
-from db import Configuration
+from db.Connection import Configuration
 
 class Connection:
     def Server():
-        db_connection = mysql.connector.connect(
+        conn = mysql.connector.connect(
             host=Configuration.DB_SERVER,
             user=Configuration.DB_USER,
             password=Configuration.DB_PASSWORD
         )
-        return db_connection
+        return conn
 
     def Database():
-        db_connection = mysql.connector.connect(
+        conn = mysql.connector.connect(
             host=Configuration.DB_SERVER,
             user=Configuration.DB_USER,
             password=Configuration.DB_PASSWORD,
             database=Configuration.DB_NAME
         )
-        return db_connection
+        return conn
